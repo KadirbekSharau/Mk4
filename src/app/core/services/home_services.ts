8d5d2d6008f2d00d58e45eb79c7fd4b9ba463deb
KadirbekSharau <sharaukadr2001@gmail.com>
2022-01-22 22:38:02 +0500

    map

---
+++ src/app/core/services/home_services.ts
@@ -0,0 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './message_service';
+import { Token } from '@angular/compiler/src/ml_parser/tokens';
+import { Form, GetLocationInfo, MapPoint } from '../interfaces';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class HomeServices {
+    httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    private url = 'http://34.105.133.177:3001';
+    private token = '';
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService) { }
+
+    getMapPoint(mapPoint: MapPoint) {
+        const urll = `${this.url}/maps?geocode=[${mapPoint.lat}, ${mapPoint.lng}]`;
+        return this.http.get<GetLocationInfo>(urll).pipe(
+          tap(_ => this.log(`fetched hotel name=${mapPoint}`)),
+          catchError(this.handleError<GetLocationInfo>(`getHotel id=${mapPoint}`))
+        );
+    }
+
+    postForm(form: Form) {
+        return this.http.post<Form>(`${this.url}/forms`, form, this.httpOptions).pipe(
+            tap((form: Form) => this.log(`added form w/ id=${form.id}`)),
+            catchError(this.handleError<Form>('addHero'))
+          );
+    }
+
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+          console.error(error); // log to console instead
+          this.log
+          (`${operation} failed: ${error.message}`);
+          return of(result as T);
+        };
+      }
+
+    private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
+    }
+}
--- src/app/features/pages/home_page/home_page.ts
+++ src/app/features/pages/home_page/home_page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import  DG  from '2gis-maps';
+import { HomeServices } from 'src/app/core/services/home_services';
+import { Form, MapPoint, PointAddress } from 'src/app/core/interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 /* Home page component */
 @Component({
@@ -9,24 +12,63 @@ import  DG  from '2gis-maps';
 })
 export class HomePage implements OnInit {
 
-  
+  address!: PointAddress;
+  form!: FormGroup;
+  forms: Form[] = [];
   title = 'Mk4';
-  constructor() {
+  constructor(private services: HomeServices) {
 
   }
   ngOnInit() {
+    this.form = new FormGroup({
+      surname: new FormControl(null, [Validators.required]),
+      name: new FormControl(null, [Validators.required]),
+      fathername: new FormControl(null, [Validators.required]),
+      id: new FormControl(null, [Validators.required]),
+      city: new FormControl(null, [Validators.required]),
+      street: new FormControl(null, [Validators.required]),
+      house: new FormControl(null, [Validators.required]),
+      flat: new FormControl(null, [Validators.required]),
+
+  });
+
     var map;
-    DG.then(function () {
+    DG.then( () => {
       map = DG.map('map', {
           center: [54.98, 82.89],
           zoom: 13
       });
-      map.on("click", (res: any)=>console.log(res.latlng));
+      map.on("click", (res: any)=>
+        this.services.getMapPoint(res.latlng)
+        .subscribe(point => {
+          if (point.address.kind == 'house') {
+            this.form.patchValue({
+              city: point.address.components.find(el => el.kind == 'locality')!.name,
+              street: point.address.components.find(el => el.kind == 'street')!.name,
+              house: point.address.components.find(el => el.kind == 'house')!.name,
+            });
+            console.log(point)
+          }
+        })
+      );
       DG.marker([54.98, 82.89]).addTo(map).bindPopup('Вы кликнули по мне!');
-  });
+    });
   }
 
   write() {
     console.log('Hey');
+  }
+
+  onSubmit() {
+    this.services.postForm(this.form.value)
+    .subscribe(forma => {
+      console.log(this.form.value)
+      this.forms.push(forma);
+    });
+    this.form.reset();
+  }
+
+  clearForm() {
+    this.form.reset()
   }
 }
