383dbb62a2a0c35ba8bc35bcd48ce43ba44c8ac3
arafat-01 <arafat.mn00@gmail.com>
2022-01-22 23:39:47 +0600

    Forms page

--- src/app/app_component/app_component_module.ts
+++ src/app/app_component/app_component_module.ts
@@ -1,3 +1,4 @@
+import { FormsPageModule } from './../features/pages/forms_page/forms_page_module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app_component';
@@ -5,9 +6,6 @@ import { AppRoutingModule } from 'src/app/features/routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { HomePageModule } from '../features/pages/home_page/home_page_module';
-
-
-
 
 @NgModule({
   declarations: [
@@ -19,6 +17,7 @@ import { HomePageModule } from '../features/pages/home_page/home_page_module';
     HttpClientModule,
     BrowserAnimationsModule,
     HomePageModule,
+    FormsPageModule
   ],
   exports: [AppComponent],
   providers: [],
---
+++ src/app/service/form_service/form.service.ts
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Form } from 'src/app/features/components/interface/form.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+  private baseUrl = 'http://34.105.133.177:3001';
+
+  constructor(private http: HttpClient) { }
+
+  getAll (): Observable<Form[]> {
+    return this.http.get<Form[]>(this.baseUrl + '/forms/');
+  }
+
+  getById (id: number): Observable<Form> {
+    return this.http.get<Form>(this.baseUrl + '/forms/' + id + '/');
+  }
+
+  deleteById (id: number): void {
+    this.http.delete(this.baseUrl + '/forms/' + id + '/');
+  }
+}
